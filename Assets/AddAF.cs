eadd39734a0cafd13d93d350247ce36da4770777
ADMIN-GTBJ6332I\Администратор <NestlPost@Gmail.com>
2018-10-28 16:21:17 +0200

    Start

---
+++ Assets/AddAF.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddAF : MonoBehaviour {
+    public static GameObject obj = null;
+    public string ANDROID_PACKAGE_NAME_HERE;
+    // Use this for initialization
+    void Start()
+    {
+        if (obj == null)
+        {
+            obj = this.gameObject;
+            DontDestroyOnLoad(this.gameObject);
+
+            /* Mandatory - set your AppsFlyer’s Developer key. */
+            AppsFlyer.setAppsFlyerKey("GQCQdRAEjrJcXTfZHVtw77");
+            /* For detailed logging */
+            /* AppsFlyer.setIsDebug (true); */
+            #if UNITY_IOS
+            /* Mandatory - set your apple app ID
+            NOTE: You should enter the number only and not the "ID" prefix */
+            AppsFlyer.setAppID ("YOUR_APP_ID_HERE");
+            AppsFlyer.trackAppLaunch ();
+            #elif UNITY_ANDROID
+            /* Mandatory - set your Android package name */
+            AppsFlyer.setAppID(ANDROID_PACKAGE_NAME_HERE);
+            /* For getting the conversion data in Android, you need to add the "AppsFlyerTrackerCallbacks" listener.*/
+            AppsFlyer.init("GQCQdRAEjrJcXTfZHVtw77", "AppsFlyerTrackerCallbacks");
+            #endif
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/ColScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ColScript : MonoBehaviour {
+    public GameObject obj;
+    public int r, g, b;
+    public float sec;
+    public int frame;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        sec += Time.deltaTime;
+        frame += 1;
+
+        r = Random.Range(0, 256);
+        g = Random.Range(0, 256);
+        b = Random.Range(0, 256);
+        obj.GetComponent<Image>().color = new Color32((byte)r, (byte)g, (byte)b, 255);
+    }
+
+    private void Awake()
+    {
+        Application.targetFrameRate = 60;
+    }
+}
